f8b98110bb4b2de01820f77cea1bf762eec177b8
Anthony <anthonygmi.14@gmail.com>
2023-05-16 21:34:26 -0300

    version string

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+ namespace contenedor
+ {
+    class program
+    {
+        static void Main (string [] args)
+        {
+            Console.Write("Ingrese un dato:");
+            string entrada = Console.ReadLine();
+
+            while (entrada != "99")
+            {
+                Console.WriteLine("Muestre en pantalla: "+entrada);
+                
+                Console.Write("Ingrese un dato: ");
+                entrada = Console.ReadLine();
+            }
+        }
+    }
+ }
