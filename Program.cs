a7985d033cfc9aa843a09802159f3e912cca0030
Anthony <anthonygmi.14@gmail.com>
2023-05-16 21:46:56 -0300

    version numerica

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@
         static void Main (string [] args)
         {
             Console.Write("Ingrese un dato:");
-            string entrada = Console.ReadLine();
+            int entrada = int.Parse(Console.ReadLine());
 
-            while (entrada != "99")
+            while (entrada != 99)
             {
                 Console.WriteLine("Muestre en pantalla: "+entrada);
                 
                 Console.Write("Ingrese un dato: ");
-                entrada = Console.ReadLine();
+                entrada = int.Parse(Console.ReadLine());
             }
         }
     }
